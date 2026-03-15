3901fd524979621c9de92cb938860578238801c6
Ola <aleksandra.reja19@gmail.com>
2026-03-15 22:24:12 +0100

    Email views updated

--- packages/Webkul/NewTheme/src/Resources/views/emails/customers/email-verification.blade.php
+++ packages/Webkul/NewTheme/src/Resources/views/emails/customers/email-verification.blade.php
@@ -3,7 +3,7 @@
 <div style="background:#ffffff; padding:45px 35px; border-top:1px solid #e4e4e7; border-bottom:1px solid #e4e4e7; text-align:center;">
 
     <p style="font-weight:700; font-size:24px; color:#111827; line-height:32px; margin:0 0 24px 0;">
-        @lang('shop::app.emails.dear', ['customer_name' => $customer->name]), 👋
+        @lang('shop::app.emails.dear', ['customer_name' => $customer->name]),
     </p>
 
     <p style="font-size:16px; color:#4B5563; line-height:26px; margin:0 0 12px 0;">
