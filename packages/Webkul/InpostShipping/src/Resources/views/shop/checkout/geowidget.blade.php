0662ee47b9ff7e3be28f0f33feb65e400e353cf4
Ola <aleksandra.reja19@gmail.com>
2026-03-18 09:28:20 +0100

    Inpost shipping method updated

--- packages/Webkul/InpostShipping/src/Resources/views/shop/checkout/geowidget.blade.php
+++ packages/Webkul/InpostShipping/src/Resources/views/shop/checkout/geowidget.blade.php
@@ -44,3 +44,39 @@
     </div>
 </div>
 
+
+
+
+<div id="inpost-widget-wrapper" style="display:none" class="mt-4 p-4 border rounded bg-white">
+    <h3 class="font-semibold mb-2">📦 Wybierz paczkomat</h3>
+
+    <div id="inpost-selected" class="{{ $savedPointId ? '' : 'hidden' }}">
+        <p><b id="inpost-point-name">{{ $savedPointId }}</b></p>
+        <p id="inpost-point-address">{{ $savedPointAddress }}</p>
+
+        <button type="button" onclick="inpostOpenWidget()" class="mt-2 text-blue-600 underline">
+            Zmień paczkomat
+        </button>
+    </div>
+
+    <button
+        id="inpost-open-btn"
+        type="button"
+        onclick="inpostOpenWidget()"
+        class="{{ $savedPointId ? 'hidden' : '' }} mt-2 px-4 py-2 bg-yellow-400 rounded"
+    >
+        Wybierz paczkomat
+    </button>
+</div>
+
+<div id="inpost-modal" style="display:none" class="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+    <div class="bg-white w-full max-w-4xl h-[80vh] rounded shadow">
+        <div class="flex justify-between p-3 border-b">
+            <span>📦 Paczkomaty InPost</span>
+            <button onclick="inpostCloseWidget()">X</button>
+        </div>
+
+        <div id="inpost-map" style="height:100%"></div>
+    </div>
+</div>
+
