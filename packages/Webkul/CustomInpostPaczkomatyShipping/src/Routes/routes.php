91e5f0df10bfded6d2c096ada020ea4c415ebc22
Ola <aleksandra.reja19@gmail.com>
2026-03-23 20:23:15 +0100

    Errors fixed

--- packages/Webkul/CustomInpostPaczkomatyShipping/src/Routes/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Webkul\CustomInpostPaczkomatyShipping\Http\Controllers\InpostPaczkomatyController;
-
-Route::group(['middleware' => ['web']], function () {
-    Route::post('inpost/save_locker', [InpostPaczkomatyController::class, 'store'])
-        ->name('inpost.save_locker');
-});
